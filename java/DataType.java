7c76d4cce9da157aef3e1073d938bafdb3254ed9
sunpxyz <sunpxyz@126.com>
2021-09-13 22:44:47 +0800

    Create DataType.java

---
+++ java/DataType.java
@@ -0,0 +1,21 @@
+public class DataType {
+    public static void main(String[] args) {
+        byte tmpByte = 1;
+        short tmpShort = 1;
+        int tmpInt = 1;
+        long tmpLong = 1L;
+        float tmpFloat = 1.0F;
+        double tmpDouble = 1.0;
+        char tmpChar ='a';
+        boolean tmpBoolean = true;
+
+        System.out.println(tmpByte);
+        System.out.println(tmpShort);
+        System.out.println(tmpInt);
+        System.out.println(tmpLong);
+        System.out.println(tmpFloat);
+        System.out.println(tmpDouble);
+        System.out.println(tmpChar);
+        System.out.println(tmpBoolean);
+    }
+}
